bbfc41f9e5d9895e7848bc2745619f31a3afd570
rlopez <rlopezdev@gmail.com>
2021-11-26 17:48:04 +0100

    suppression du timeout pour parallax

--- dist/assets/js/contact.js
+++ dist/assets/js/contact.js
@@ -1,9 +1,6 @@
 $(document).ready(() => {
-    setTimeout(() => {
-        console.log($('.page-title-container'))
-        $('.page-title-container').parallax({
-            imageSrc: 'assets/img/background-contact.jpg',
-            positionY: 'auto'
-        });
-    }, 100)
+    $('.page-title-container').parallax({
+        imageSrc: 'assets/img/background-contact.jpg',
+        positionY: 'auto'
+    });
 })
--- dist/assets/js/page-habitations.js
+++ dist/assets/js/page-habitations.js
@@ -1,17 +1,15 @@
 $(document).ready(() => {
-    setTimeout(() => {
-        $('.call-to-action').parallax({
-            imageSrc: 'assets/img/background-logo.jpg',
-            positionY: 'auto'
-        });
-        $('#follow-block').parallax({
-            imageSrc: 'assets/img/background-homapge-social.jpg',
-            positionY: 'auto'
-        });
-        console.log($('.page-title-container'))
-        $('.page-title-container').parallax({
-            imageSrc: 'assets/img/background-assurance-habitation.jpg',
-            positionY: 'auto'
-        });
-    }, 100)
+    $('.call-to-action').parallax({
+        imageSrc: 'assets/img/background-logo.jpg',
+        positionY: 'auto'
+    });
+    $('#follow-block').parallax({
+        imageSrc: 'assets/img/background-homapge-social.jpg',
+        positionY: 'auto'
+    });
+    console.log($('.page-title-container'))
+    $('.page-title-container').parallax({
+        imageSrc: 'assets/img/background-assurance-habitation.jpg',
+        positionY: 'auto'
+    });
 })
