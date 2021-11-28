930180129d54f0e139074f961737672fa3dcb958
rlopez <rlopezdev@gmail.com>
2021-11-28 22:30:14 +0100

    WIP: resonsive > burger menu ok sur toutes les pags

--- dist/assets/js/page-habitations.js
+++ dist/assets/js/page-habitations.js
@@ -12,4 +12,22 @@ $(document).ready(() => {
         imageSrc: 'assets/img/background-assurance-habitation.jpg',
         positionY: 'auto'
     });
+    /* Burger nav */
+    const burgerNavBtn = document.getElementById('burger-nav-btn')
+    const burgerNavList = document.getElementById('burger-nav-list')
+    burgerNavBtn.onclick = function() {
+        if (burgerNavBtn.classList.contains('closed')) {
+            burgerNavBtn.classList.remove('closed')
+            burgerNavBtn.classList.add('open')
+
+            burgerNavList.classList.remove('hidden')
+            burgerNavList.classList.add('visible')
+        } else if (burgerNavBtn.classList.contains('open')) {
+            burgerNavBtn.classList.remove('open')
+            burgerNavBtn.classList.add('closed')
+
+            burgerNavList.classList.remove('visible')
+            burgerNavList.classList.add('hidden')
+        }
+    }
 })
